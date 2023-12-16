a06025cc6fa5c141876b5ddbc9cd6c43024f9578
Alexey Grishankov <domian844@gmail.com>
2023-12-16 07:05:50 +0300

    add template

--- src/main/kotlin/com/glex/deps/services/update_mod/routes/GetStatusBackend.kt
+++ src/main/kotlin/com/glex/deps/services/update_mod/routes/GetStatusBackend.kt
@@ -3,11 +3,23 @@ package com.glex.deps.services.update_mod.routes
 import com.glex.deps.core.Response
 import io.ktor.http.*
 import io.ktor.server.application.*
+import io.ktor.server.html.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.html.body
+import kotlinx.html.h1
 
 fun Routing.addGetStatusBackend() {
     get("status") {
         call.respond(HttpStatusCode.OK, Response(data = "Status: OK"))
+    }
+    get("/") {
+        call.respondHtml(HttpStatusCode.OK) {
+            body {
+                h1 {
+                    + "IMPACT UPDATER"
+                }
+            }
+        }
     }
 }
